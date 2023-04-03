7e4b152bac31580bf5d9ed0eb284b776d1aa13aa
zzq <i@zhangzqs.cn>
2023-04-03 10:35:01 +0800

    add rename test by mock server

--- api.v7/kodo/bucket_operator_test.go
+++ api.v7/kodo/bucket_operator_test.go
@@ -2,7 +2,12 @@ package kodo
 
 import (
 	"bytes"
+	"context"
+	"encoding/base64"
 	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -64,4 +69,53 @@ func TestBucketOperator(t *testing.T) {
 
 	stat, err = bucket.Stat(nil, key)
 	assert.Error(t, err)
+}
+
+func TestBucket_Rename(t *testing.T) {
+	checkSkipTest(t)
+
+	ctx := context.Background()
+	key := "test.txt"
+	newKey := "test_new.txt"
+
+	assert.NoError(t, bucket.Put(
+		ctx, nil,
+		key, bytes.NewReader(nil), 0,
+		nil,
+	))
+
+	//oldApiServerHost := bucket.Conn.APIHost
+
+	// 构造一个测试服务器，将mock api server下的rename接口代替为move接口
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		decodeURI := func(uri string) string {
+			e, err := base64.URLEncoding.DecodeString(uri)
+			assert.NoError(t, err)
+			return string(e)
+		}
+		if r.Method == "POST" && strings.HasPrefix(r.URL.Path, "/rename") {
+			strs := strings.Split(r.URL.Path, "/")
+			e1, e2 := decodeURI(strs[2]), decodeURI(strs[3])
+			_, keySrc := func() (string, string) {
+				strs := strings.Split(e1, ":")
+				return strs[0], strs[1]
+			}()
+			_, keyDest := func() (string, string) {
+				strs := strings.Split(e2, ":")
+				return strs[0], strs[1]
+			}()
+			assert.NoError(t, bucket.Move(ctx, keySrc, keyDest))
+			w.WriteHeader(200)
+			return
+		}
+	}))
+	defer ts.Close()
+
+	bucket.Conn.APIHost = ts.URL
+
+	assert.NoError(t, bucket.Rename(ctx, key, newKey))
+	_, err := bucket.Stat(ctx, key)
+	assert.Error(t, err)
+
+	bucket.Delete(ctx, newKey)
 }
