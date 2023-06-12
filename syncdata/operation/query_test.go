7913b2d01e097eb9703ceb8c6fb659344beaa24f
zzq <i@zhangzqs.cn>
2023-06-12 18:49:01 +0800

    add query mock test

--- syncdata/operation/query_test.go
+++ syncdata/operation/query_test.go
@@ -3,17 +3,78 @@ package operation
 import (
 	"github.com/stretchr/testify/assert"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestNewQueryer(t *testing.T) {
-	assert.NotNil(t, NewQueryer(getConfig1()))
+const mockResponse = `
+{
+    "hosts": [
+        {
+            "region": "z0",
+            "ttl": 86400,
+            "io": {
+                "domains": [
+                    "iovip.qbox.me"
+                ]
+            },
+            "up": {
+                "domains": [
+                    "upload.qiniup.com",
+                    "up.qiniup.com"
+                ],
+                "old": [
+                    "upload.qbox.me",
+                    "up.qbox.me"
+                ]
+            },
+            "uc": {
+                "domains": [
+                    "uc.qbox.me"
+                ]
+            },
+            "rs": {
+                "domains": [
+                    "rs-z0.qbox.me"
+                ]
+            },
+            "rsf": {
+                "domains": [
+                    "rsf-z0.qbox.me"
+                ]
+            },
+            "api": {
+                "domains": [
+                    "api.qiniu.com"
+                ]
+            },
+            "s3": {
+                "domains": [
+                    "s3-cn-east-1.qiniucs.com"
+                ],
+                "region_alias": "cn-east-1"
+            }
+        }
+    ],
+    "ttl": 86400
 }
+`
 
-func TestQueryer_QueryApiServerHosts(t *testing.T) {
-	q := NewQueryer(getConfig1())
-	a := q.QueryApiServerHosts(true)
+func TestQueryer(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(mockResponse))
+		assert.NoError(t, err)
+	}))
+
+	queryer := Queryer{
+		ak:      "mock_ak",
+		bucket:  "mock_bucket",
+		ucHosts: []string{server.URL},
+		client:  server.Client(),
+	}
+
+	a := queryer.QueryUpHosts(true)
 	log.Println(a)
-	b := q.QueryUpHosts(true)
-	log.Println(b)
 }
