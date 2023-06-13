4937e78aabfc5d27101df31da9e7f116bda4f6d6
zzq <i@zhangzqs.cn>
2023-06-13 10:53:26 +0800

    add query timeout mock test

--- syncdata/operation/query_test.go
+++ syncdata/operation/query_test.go
@@ -6,9 +6,10 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
-const mockResponse = `
+const mockQueryResponse = `
 {
     "hosts": [
         {
@@ -61,20 +62,67 @@ const mockResponse = `
 }
 `
 
-func TestQueryer(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func assertQueryByTimeout(t *testing.T, serverDialDuration, serverDuration, clientDialTimeout, clientTimeout time.Duration, expectErr bool) {
+	server := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("server received request")
+		time.Sleep(serverDialDuration)
 		w.WriteHeader(http.StatusOK)
-		_, err := w.Write([]byte(mockResponse))
+		_, err := w.Write([]byte(mockQueryResponse))
 		assert.NoError(t, err)
+		time.Sleep(serverDuration)
 	}))
+	defer server.Close()
 
+	server.Start()
+
+	log.Println("server started url: ", server.URL)
 	queryer := Queryer{
-		ak:      "mock_ak",
-		bucket:  "mock_bucket",
-		ucHosts: []string{server.URL},
-		client:  server.Client(),
+		ak:          "mock_ak",
+		bucket:      "mock_bucket",
+		ucHosts:     []string{server.URL},
+		ucTimeout:   clientTimeout,
+		dialTimeout: clientDialTimeout,
+	}
+	c, err := queryer.mustQuery()
+	if expectErr {
+		assert.Error(t, err)
+		return
+	} else {
+		assert.NoError(t, err)
+	}
+	assert.GreaterOrEqual(t, time.Now().Add(86400*time.Second).Compare(c.CacheExpiredAt), 0)
+
+	assert.Equal(t, int64(86400), c.CachedHosts.Hosts[0].Ttl)
+
+	assert.Equal(t, "iovip.qbox.me", c.CachedHosts.Hosts[0].Io.Domains[0])
+	assert.Equal(t, "upload.qiniup.com", c.CachedHosts.Hosts[0].Up.Domains[0])
+	assert.Equal(t, "up.qiniup.com", c.CachedHosts.Hosts[0].Up.Domains[1])
+
+	assert.Equal(t, "rs-z0.qbox.me", c.CachedHosts.Hosts[0].Rs.Domains[0])
+	assert.Equal(t, "rsf-z0.qbox.me", c.CachedHosts.Hosts[0].Rsf.Domains[0])
+	assert.Equal(t, "api.qiniu.com", c.CachedHosts.Hosts[0].ApiServer.Domains[0])
+}
+
+func TestQueryer(t *testing.T) {
+	cases := []struct {
+		serverDialDuration int
+		serverDuration     int
+		clientDialTimeout  int
+		clientTimeout      int
+		expectErr          bool
+	}{
+		// 客户端设置的超时时间小于服务端的处理时间，客户端会超时
+		{100, 100, 50, 50, true},
+		// 客户端设置的超时时间大于服务端的处理时间，客户端不会超时
+		{50, 200, 100, 300, false},
 	}
 
-	a := queryer.QueryUpHosts(true)
-	log.Println(a)
+	for _, c := range cases {
+		assertQueryByTimeout(
+			t,
+			time.Duration(c.serverDialDuration)*time.Millisecond, time.Duration(c.serverDuration)*time.Millisecond,
+			time.Duration(c.clientDialTimeout)*time.Millisecond, time.Duration(c.clientTimeout)*time.Millisecond,
+			c.expectErr,
+		)
+	}
 }
